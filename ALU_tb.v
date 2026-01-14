050ac95961365c8ddf23bf7aadc3a330caee3c5e
JaishyamReddivari <167150149+JaishyamReddivari@users.noreply.github.com>
2026-01-14 09:04:10 -0500

    Update ALU_tb.v

--- ALU_tb.v
+++ ALU_tb.v
@@ -1,90 +1,175 @@
-`timescale 1us/1ns
-module ALU_tb();
-   parameter B_W = 8;
-   reg [3:0] opcode;
-   reg [B_W-1:0] a, b;
-   reg c_in;
-   wire [B_W-1:0] y;
-   wire c_out, borrow, invalid_op;
-   wire zero, parity;
+interface alu_if #(parameter B_W = 8);
+    logic [B_W-1:0] a, b;
+    logic c_in;
+    logic [3:0] opcode;
+    logic [B_W-1:0] y;
+    logic c_out, borrow, invalid_op;
+    logic zero, parity;
+endinterface
 
-   localparam OP_ADD = 1;
-   localparam OP_ADD_CARRY = 2;
-   localparam OP_SUB = 3;
-   localparam OP_INC = 4;
-   localparam OP_DEC = 5;
-   localparam OP_AND = 6;
-   localparam OP_NOT = 7;   
-   localparam OP_ROL = 8;
-   localparam OP_ROR = 9;
-   integer success_count = 0, error_count = 0, test_count = 0, i = 0;
+class alu_txn;
+    rand bit [7:0] a, b;
+    rand bit c_in;
+    rand bit [3:0] opcode;
 
-ALU
-   #(.B_W(B_W))
-ALU(
-   .a(a), .b(b), .c_in(c_in), .opcode(opcode),
-   .y(y), .c_out(c_out), .borrow(borrow), 
-   .invalid_op(invalid_op), .parity(parity),
-   .zero(zero)
-);
+    bit [7:0] y_obs;
+    bit c_out_obs, borrow_obs, invalid_op_obs;
+    bit zero_obs, parity_obs;
+endclass
 
-   function [B_W+4:0] model_ALU(input [3:0] opcode, input [B_W-1:0] a, input [B_W-1:0] b, input c_in);
-      reg [B_W-1:0] y;
-      reg c_out;
-      reg borrow, zero, parity, invalid_op;
+class alu_driver;
+    virtual alu_if vif;
+    mailbox #(alu_txn) gen2drv;
 
-      begin
-         y = 0; c_out = 0; borrow = 0; invalid_op = 0;
-         case(opcode)
-            OP_ADD : begin {c_out, y} = a + b; end
-            OP_ADD_CARRY : begin {c_out, y} = a + b + c_in; end
-            OP_SUB : begin {borrow, y} = a - b; end
-            OP_INC : begin {c_out, y} = a + 1'b1; end
-            OP_DEC : begin {borrow, y} = a - 1'b1; end
-            OP_AND : begin y = a & b; end
-            OP_NOT : begin y = ~a ; end
-            OP_ROL : begin y = {a[B_W-2:0], a[B_W-1]}; end
-            OP_ROR : begin y = {a[0], a[B_W-1:1]}; end
-            default : begin invalid_op = 1; y = 0; c_out = 0; borrow = 0; end
-          endcase
-          
-          parity = ^y;
-          zero = (y == 0);
-          model_ALU = {invalid_op, parity, zero, borrow, c_out, y};
-       end
-   endfunction
+    function new(virtual alu_if vif, mailbox #(alu_txn) gen2drv);
+        this.vif = vif;
+        this.gen2drv = gen2drv;
+    endfunction
 
-   task compare_data(input [B_W+4:0] expected_ALU, input [B_W+4:0] observed_ALU);
-      begin
-         if(expected_ALU === observed_ALU) begin
-            $display($time, " SUCCESS \t EXPECTED invalid_op=%0d, parity=%b, zero=%b, c_out=%b, y=%b", expected_ALU[B_W+4], expected_ALU[B_W+3], expected_ALU[B_W+2], expected_ALU[B_W+1], expected_ALU[B_W], expected_ALU[B_W-1:0]);          
-            $display($time, "         \t OBSERVED invalid_op=%0d, parity=%b, zero=%b, c_out=%b, y=%b", observed_ALU[B_W+4], observed_ALU[B_W+3], observed_ALU[B_W+2], observed_ALU[B_W+1], observed_ALU[B_W], observed_ALU[B_W-1:0]);
-            success_count = success_count + 1;
+    task run();
+        alu_txn tx;
+        forever begin
+            gen2drv.get(tx);
+            vif.a      = tx.a;
+            vif.b      = tx.b;
+            vif.c_in   = tx.c_in;
+            vif.opcode = tx.opcode;
+            #5;
+        end
+    endtask
+endclass
 
-         end else begin
-             $display($time, " ERROR \t EXPECTED invalid_op=%0d, parity=%b, zero=%b, c_out=%b, y=%b", expected_ALU[B_W+4], expected_ALU[B_W+3], expected_ALU[B_W+2], expected_ALU[B_W+1], expected_ALU[B_W], expected_ALU[B_W-1:0]);          
-            $display($time, "         \t OBSERVED invalid_op=%0d, parity=%b, zero=%b, c_out=%b, y=%b", observed_ALU[B_W+4], observed_ALU[B_W+3], observed_ALU[B_W+2], observed_ALU[B_W+1], observed_ALU[B_W], observed_ALU[B_W-1:0]);
-            error_count = error_count + 1;
-         end
-         test_count = test_count + 1;
-      end
-   endtask
+class alu_monitor;
+    virtual alu_if vif;
+    mailbox #(alu_txn) mon2scb;
 
-   initial begin
-      for (i=0; i<100; i=i+1) begin
-         opcode = $random % 10'd11;
-         a = $random;
-         b = $random;
-         c_in = $random;
+    function new(virtual alu_if vif, mailbox #(alu_txn) mon2scb);
+        this.vif = vif;
+        this.mon2scb = mon2scb;
+    endfunction
 
-         #1;
-         $display($time, " TEST%0d opcode = %0d, a = %0d, b = %0d, c_in = %0b", i, opcode, a, b, c_in);
-         compare_data(model_ALU(opcode, a, b, c_in), {invalid_op, parity, zero,borrow, c_out, y});
+    task run();
+        alu_txn tx;
+        forever begin
+            #5;
+            tx = new();
+            tx.a = vif.a;
+            tx.b = vif.b;
+            tx.c_in = vif.c_in;
+            tx.opcode = vif.opcode;
 
-         #2;
-      end
-      
-      $display($time, " TEST RESULTS success_count = %0d, error_count = %0d, test_count = %0d", success_count, error_count, test_count);
-      #40 $stop;
-   end
-endmodule 
+            tx.y_obs = vif.y;
+            tx.c_out_obs = vif.c_out;
+            tx.borrow_obs = vif.borrow;
+            tx.invalid_op_obs = vif.invalid_op;
+            tx.zero_obs = vif.zero;
+            tx.parity_obs = vif.parity;
+
+            mon2scb.put(tx);
+        end
+    endtask
+endclass
+
+class alu_scoreboard;
+    mailbox #(alu_txn) mon2scb;
+
+    function new(mailbox #(alu_txn) mon2scb);
+        this.mon2scb = mon2scb;
+    endfunction
+
+    task run();
+        alu_txn tx;
+        bit [8:0] exp;
+
+        forever begin
+            mon2scb.get(tx);
+
+            case (tx.opcode)
+                1:  exp = tx.a + tx.b;
+                2:  exp = tx.a + tx.b + tx.c_in;
+                3:  exp = tx.a - tx.b;
+                4:  exp = tx.a + 1;
+                5:  exp = tx.a - 1;
+                6:  exp = tx.a & tx.b;
+                7:  exp = ~tx.a;
+                8:  exp = {tx.a[6:0], tx.a[7]};
+                9:  exp = {tx.a[0], tx.a[7:1]};
+                default: exp = 0;
+            endcase
+
+            if (tx.y_obs !== exp[7:0]) begin
+                $error("ALU ERROR | opcode=%0d a=%0h b=%0h exp=%0h got=%0h",
+                       tx.opcode, tx.a, tx.b, exp[7:0], tx.y_obs);
+            end
+        end
+    endtask
+endclass
+
+class alu_env;
+    alu_driver drv;
+    alu_monitor mon;
+    alu_scoreboard scb;
+
+    mailbox #(alu_txn) gen2drv;
+    mailbox #(alu_txn) mon2scb;
+
+    function new(virtual alu_if vif);
+        gen2drv = new();
+        mon2scb = new();
+        drv = new(vif, gen2drv);
+        mon = new(vif, mon2scb);
+        scb = new(mon2scb);
+    endfunction
+
+    task run();
+        fork
+            drv.run();
+            mon.run();
+            scb.run();
+        join_none
+    endtask
+endclass
+
+class alu_test;
+    alu_env env;
+
+    function new(virtual alu_if vif);
+        env = new(vif);
+    endfunction
+
+    task run();
+        alu_txn tx;
+        env.run();
+
+        repeat (100) begin
+            tx = new();
+            tx.randomize() with { opcode inside {[1:9]}; };
+            env.gen2drv.put(tx);
+            #10;
+        end
+
+        #50 $finish;
+    endtask
+endclass
+
+module tb;
+    alu_if intf();
+
+    ALU dut (
+        .a(intf.a),
+        .b(intf.b),
+        .c_in(intf.c_in),
+        .opcode(intf.opcode),
+        .y(intf.y),
+        .c_out(intf.c_out),
+        .borrow(intf.borrow),
+        .invalid_op(intf.invalid_op),
+        .zero(intf.zero),
+        .parity(intf.parity)
+    );
+
+    initial begin
+        alu_test test = new(intf);
+        test.run();
+    end
+endmodule
